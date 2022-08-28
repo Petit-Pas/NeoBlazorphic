c4dd851c914144264fdcc30597a419cad064812a
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-08-28 21:52:53 +0200

    Cleaned code for CircularSelectorButton

---
+++ NeoBlazorphic/Extensions/BaseTypes/NumberExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoBlazorphic.Extensions.BaseTypes
+{
+    public static class NumberExtensions
+    {
+        public static string ToInvariantString(this double num, int amountOfDigit = 2)
+        {
+            var format = "0." + new string ('0', amountOfDigit);
+
+            return num.ToString(format, System.Globalization.CultureInfo.InvariantCulture);
+        }
+        
+    }
+}
