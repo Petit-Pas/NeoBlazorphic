09786901286afc44ad88d764b1bb5eeb5b3e35b2
GameboyColor32 <victor.vindevogel@epitech.eu>
2022-10-07 14:12:58 +0100

    (+) very basic component who implements SelectableItemList (wip)

---
+++ NeoBlazorphic/Components/Inputs/DropDownLists/DropDownList.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using NeoBlazorphic.Models.SelectableItems;
+
+namespace NeoBlazorphic.Components.Inputs.DropDownLists
+{
+	public partial class DropDownList<T> : UiComponentBase
+    {
+        [Parameter, EditorRequired]
+        public SelectableItemList<T> Items { get; set; } = SelectableItemList<T>.Empty;
+    }
+}
---
+++ NeoBlazorphic/Components/Inputs/DropDownLists/DropDownListButton.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using NeoBlazorphic.Models.SelectableItems;
+using NeoBlazorphic.UserInteraction.Mouse.BaseComponents;
+
+namespace NeoBlazorphic.Components.Inputs.DropDownLists
+{
+    public partial class DropDownListButton<T> : MouseInteractiveBaseComponent
+    {
+        [Parameter, EditorRequired]
+        public SelectableItem<T> Item { get; set; } = SelectableItem<T>.Empty;
+    }
+}
