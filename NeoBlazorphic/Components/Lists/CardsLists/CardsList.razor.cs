2ec2bdcdf4c7f4b48a44bfcececc2abece68d2c8
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-10-18 20:46:15 +0200

    basic display implementation, ugly but 1st version that works with scroll properly

---
+++ NeoBlazorphic/Components/Lists/CardsLists/CardsList.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NeoBlazorphic.Components.Lists.CardsLists
+{
+    public partial class CardsList<T>
+    {
+        [Parameter, EditorRequired]
+        public List<T>? Items { get; set; }
+
+        [Parameter, EditorRequired]
+        public RenderFragment<T>? ItemRenderFragment { get; set; }
+
+        public void DebugClicked()
+        {
+            ;
+        }
+    }
+}
