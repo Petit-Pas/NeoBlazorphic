adb119976b1df5dabdb8eccdf3e26128d4feea23
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-10-21 14:11:49 +0200

    splitted the list with a listItem component that is reused in a loop

--- NeoBlazorphic/Components/Lists/CardsLists/CardsList.razor.cs
+++ NeoBlazorphic/Components/Lists/CardsLists/CardsList.razor.cs
@@ -1,18 +1,38 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop.Infrastructure;
+
 
 namespace NeoBlazorphic.Components.Lists.CardsLists
 {
     public partial class CardsList<T>
     {
+
         [Parameter, EditorRequired]
         public List<T>? Items { get; set; }
 
         [Parameter, EditorRequired]
         public RenderFragment<T>? ItemRenderFragment { get; set; }
 
-        public void DebugClicked()
+        [Parameter]
+        public Func<T, string, bool>? Filter { get; set; }
+
+        public string? SearchBarQuery
         {
-            ;
+            get => _searchBarQuery;
+            set => _searchBarQuery = value;
+        }
+        private string? _searchBarQuery = "";
+
+        private IEnumerable<T>? DisplayableItems()
+        {
+            if (Filter == null || string.IsNullOrEmpty(SearchBarQuery))
+            {
+                return Items;
+            }
+
+            return Items?.Where(x => Filter(x, SearchBarQuery));
         }
     }
 }
---
+++ NeoBlazorphic/Components/Lists/CardsLists/CardsListItem.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using NeoBlazorphic.StyleParameters;
+using NeoBlazorphic.UserInteraction.Mouse.BaseComponents;
+
+namespace NeoBlazorphic.Components.Lists.CardsLists
+{
+    public partial class CardsListItem<T> : MouseInteractiveBaseComponent
+    {
+        [Parameter, EditorRequired]
+        public T? Item { get; set; }
+
+        [Parameter, EditorRequired]
+        public RenderFragment<T>? ItemRenderFragment { get; set; }
+
+        private BackgroundShape CardShape { get; set; } = BackgroundShape.Flat;
+
+        protected override Task OnMouseOver(MouseEventArgs args)
+        {
+            CardShape = BackgroundShape.Concave;
+            return base.OnMouseOver(args);
+        }
+
+        protected override Task OnMouseOut(MouseEventArgs args)
+        {
+            CardShape = BackgroundShape.Flat;
+            return base.OnMouseOut(args);
+        }
+    }
+}
