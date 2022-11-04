85ef9661bce594f800249fbe81a2269e03a3f041
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-11-04 11:45:25 +0100

    extracted interface from class file

--- NeoBlazorphic/Models/SelectableItems/SelectableItemList.cs
+++ NeoBlazorphic/Models/SelectableItems/SelectableItemList.cs
@@ -2,14 +2,6 @@
 
 namespace NeoBlazorphic.Models.SelectableItems;
 
-public interface ISelectableItemList
-{
-    void ResetSelected(ISelectableItem? selectedItem);
-    bool HasSelectedItem(int amount = 1);
-
-    event EventHandler SelectionUpdated;
-}
-
 public class SelectableItemList<T> : List<SelectableItem<T>>, ISelectableItemList
 {
     public static readonly SelectableItemList<T> Empty = new();
