c7ea11db17a74b9aa4598bee76c4b223b8b8af9b
Benoit Hoffman <benoit.hoffman@satellit.be>
2022-11-14 11:08:49 +0100

    very basic, non styled component that has fluentValidation and update on keystroke

---
+++ NeoBlazorphic/Components/Inputs/Fields/BaseFields/BaseTextField.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+
+namespace NeoBlazorphic.Components.Inputs.Fields.BaseFields
+{
+    public partial class BaseTextField : InputBase<string>
+    {
+        [Parameter, EditorRequired] public Expression<Func<string>> ValidationFor { get; set; } = default!;
+        [Parameter] public string? Label { get; set; }
+        [Parameter] public string? Placeholder { get; set; }
+
+        private string Id { get; set; } = Guid.NewGuid().ToString();
+
+        protected override bool TryParseValueFromString(string? value, out string result, out string? validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
