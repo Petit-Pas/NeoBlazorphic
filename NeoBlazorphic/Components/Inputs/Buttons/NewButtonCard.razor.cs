d0d625b032886eafbb63b1203e3940249af1c1ad
Benoit Hoffman <benoit.hoffman@satellit.be>
2023-02-06 22:20:57 +0100

    Redid button cards that use CSS properly

---
+++ NeoBlazorphic/Components/Inputs/Buttons/NewButtonCard.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using NeoBlazorphic.Extensions.BaseTypes;
+using NeoBlazorphic.StyleParameters;
+
+namespace NeoBlazorphic.Components.Inputs.Buttons
+{
+    public partial class NewButtonCard : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public ColorTheme ColorTheme { get; set; } = ColorTheme.Base;
+
+        [Parameter]
+        public BorderRadius BorderRadius { get; set; } = BorderRadius.Default;
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnMouseClickCallBack { get; set; }
+
+        protected virtual async Task OnMouseClick(MouseEventArgs args)
+        {
+            await OnMouseClickCallBack.InvokeAsync(args);
+        }
+
+        // UI Methods
+        public string GetColorTheme => ColorTheme.GetCssClass();
+
+        
+
+    }
+}
